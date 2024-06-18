2397b5b3d2a1cefeb22d4a45b1bc65aaf201e391
nbeverton <104376567+nbeverton@users.noreply.github.com>
2024-06-18 19:46:07 -0300

    Update
    Main page layout update.
    A basic directory creaton.

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-pricing.component.html',
+  styleUrl: './card-pricing.component.css'
+})
+export class CardPricingComponent {
+
+}
